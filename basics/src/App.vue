ad9bc5e56caec29eef3b2d1378611f967661128d
ani3321r <ani3321r@gmail.com>
2025-01-17 22:59:01 +0530

    some updates

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -2,10 +2,12 @@
   import Navbar from './components/Navbar.vue';
   import Hero from './components/Hero.vue';
   import HomeCards from './components/HomeCards.vue';
+  import Topics from '@/components/Topics.vue';
 </script>
 
 <template>
   <Navbar/>
   <Hero />
   <HomeCards />
+  <Topics />
 </template>
---
+++ basics/src/components/Topics.vue
@@ -0,0 +1,18 @@
+<script setup>
+import topicData from '@/topics.json';
+</script>
+
+<template>
+  <section class="bg-blue-50 px-4 py10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Topics
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div v-for="topic in topicData" :key="topic.id">
+          {{ topic.title }}
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
